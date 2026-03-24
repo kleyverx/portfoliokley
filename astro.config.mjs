f6d4b6026b72e4edc9b95b79475c01d2112a3087
Kleyver <kleyvercell2@gmail.com>
2026-03-24 19:55:41 -0400

    fix: corregir base path para Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kleyverx.github.io',
+  base: '/portfoliokley',
   devToolbar: {
     enabled: false
   },
